42effe54593269677b3b2a56246627e69fcdb25a
BolNick <BolNick09RUS@gmail.com>
2025-01-31 16:21:40 +0300

    Добавьте файлы проекта.

---
+++ FTPDownloadFile/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using System.Net;
+
+namespace FTPDownloadFile
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            string ftpUrl = "ftp://ftp.dlptest.com/";
+            string user = "dlpuser";
+            string password = "rNrKYTX9g7z3RgJRmxWuGHbeu";
+            string filePath = "Testfile311.txt";
+            string fileUrl = $"{ftpUrl}{filePath}";
+
+
+
+            FtpWebRequest uploadRequest = (FtpWebRequest)WebRequest.Create(fileUrl);
+            uploadRequest.Method = WebRequestMethods.Ftp.UploadFile;
+            uploadRequest.Credentials = new NetworkCredential(user, password);
+
+            Stopwatch stopwatchUpload = Stopwatch.StartNew();
+
+            byte[] fileContents;
+            using (FileStream fs = File.OpenRead(filePath))
+            {
+                fileContents = new byte[fs.Length];
+                await fs.ReadAsync(fileContents, 0, fileContents.Length);
+            }
+
+            using Stream requestStream = await uploadRequest.GetRequestStreamAsync();
+            await requestStream.WriteAsync(fileContents, 0, fileContents.Length);
+
+            stopwatchUpload.Stop();
+            Console.WriteLine("Файл успешно отправлен.");
+
+            double uploadTime = stopwatchUpload.Elapsed.TotalSeconds;
+            long fileSize = new FileInfo(filePath).Length;
+
+            double uploadSpeed = fileSize / uploadTime; 
+            Console.WriteLine($"Время отправки: {uploadTime:N2} сек.");
+            Console.WriteLine($"Скорость отправки: {uploadSpeed / 1024 / 1024:N2} Мбайт/сек.");
+
+
+            FtpWebRequest downloadRequest = (FtpWebRequest)WebRequest.Create(fileUrl);
+            downloadRequest.Method = WebRequestMethods.Ftp.DownloadFile;
+            downloadRequest.Credentials = new NetworkCredential(user, password);//удостоверение, лог-пасс
+
+
+            Stopwatch stopwatchDownload = Stopwatch.StartNew();
+            //Выполнение запроса
+            //на этой строке зависает, не пойму почему
+            using FtpWebResponse response =  (FtpWebResponse) await downloadRequest.GetResponseAsync();
+            if ((int)response.StatusCode < 100 || (int)response.StatusCode >= 400)
+                throw new InvalidOperationException("Failed status response");
+
+            Stream file = File.Create(filePath);
+            await response.GetResponseStream().CopyToAsync(file);
+
+            Console.WriteLine("Файл успешно скачен и записан");
+            
+            double downloadTime = stopwatchDownload.Elapsed.TotalSeconds;
+
+            double downloadSpeed = fileSize / downloadTime;
+            Console.WriteLine($"Время получения: {downloadTime:N2} сек.");
+            Console.WriteLine($"Скорость получения: {downloadSpeed / 1024 / 1024:N2} Мбайт/сек.");
+
+            
+        }
+    }
+}
---
+++ SendMail_SMTP/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+
+namespace SendMail_SMTP
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            MailAddress from = new MailAddress("test.smtp2@inbox.ru");
+            List<MailAddress> to =
+            [
+                new MailAddress("test.smtp2@inbox.ru"),
+                new MailAddress("BolNick09RUS@gmail.com"),
+                new MailAddress("Bolshakov199709@rambler.ru"),
+            ];
+
+            foreach (MailAddress address in to)
+            { 
+                MailMessage message = new MailMessage(from, address);
+                //message.CC.Add(new MailAddress("test.smtp2@inbox.ru"));копия
+                message.Subject = $"Тема письма для {TrimAddress(address.Address)}";
+                message.Body = @"
+                            <html>
+                            <body>
+                                <h1>Заголовок текста письма</h1>
+                                <p>текст письма</p>
+                            </body>
+                            </html>";
+                message.IsBodyHtml = true;
+                Attachment attachment = new Attachment("cage1.csv", "text/csv");
+                message.Attachments.Add(attachment);
+
+                using SmtpClient client = new SmtpClient("smtp.mail.ru");
+                client.Credentials = new NetworkCredential("test.smtp2@inbox.ru", "0DE6LbSMnLCfAkzs1xpt");
+                client.EnableSsl = true;
+
+                await client.SendMailAsync(message);
+                Console.WriteLine("Письмо отправлено");
+            }
+
+
+        }
+
+        public static string TrimAddress(string address)
+        {
+            return address.Substring(0, address.IndexOf('@'));
+        }
+    }
+}
